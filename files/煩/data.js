7f03e8a6848f52ef32c61bc6ad32ccfbd0fc2a3e
kevlh <IXD@IXDtekiiMac-67.local>
2015-01-12 18:36:42 +0800

    final

---
+++ files/煩/data.js
@@ -0,0 +1,452 @@
+﻿$axure.loadCurrentPage({
+  "url":"煩.html",
+  "generationDate":new Date(1420987575263.49),
+  "isCanvasEnabled":false,
+  "variables":["OnLoadVariable"],
+  "page":{
+    "packageId":"69fc5b1b8663410f85ad4c4b1d187909",
+    "type":"Axure:Page",
+    "name":"煩",
+    "notes":{
+},
+    "style":{
+      "baseStyle":"627587b6038d43cca051c114ac41ad32",
+      "pageAlignment":"near",
+      "fill":{
+        "fillType":"solid",
+        "color":0xFFFFFFFF},
+      "image":null,
+      "imageHorizontalAlignment":"near",
+      "imageVerticalAlignment":"near",
+      "imageRepeat":"auto",
+      "favicon":null,
+      "sketchFactor":"0",
+      "colorStyle":"appliedColor",
+      "fontName":"应用字体",
+      "borderWidth":"0"},
+    "adaptiveStyles":{
+},
+    "interactionMap":{
+},
+    "diagram":{
+      "objects":[{
+          "id":"fe97c0e65ad24ed093c427c22a879f81",
+          "label":"POO",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":false,
+          "style":{
+            "location":{
+              "x":290,
+              "y":228},
+            "size":{
+              "width":55,
+              "height":55},
+            "visible":false},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"bbd8b57a55154984a164efb41ba06ea6",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":false,
+              "style":{
+                "location":{
+                  "x":290,
+                  "y":228},
+                "size":{
+                  "width":55,
+                  "height":55},
+                "visible":false},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/煩/regen/poo_u0.png"}},
+{
+          "id":"4bcc7b8df02e44109d882218b7236041",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":182,
+              "y":120},
+            "size":{
+              "width":388,
+              "height":290}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"e07c9c65bfa14745b71122ebeae0a500",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":182,
+                  "y":120},
+                "size":{
+                  "width":388,
+                  "height":290}},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/煩/regen/u2.png"}},
+{
+          "id":"7fafc98146bd4c979b83a5b2d3af0b9d",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":120,
+              "y":10},
+            "size":{
+              "width":190,
+              "height":142}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"7dec9b7a2a9f4aaebd412a6f2b64a934",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":120,
+                  "y":10},
+                "size":{
+                  "width":190,
+                  "height":142}},
+              "adaptiveStyles":{
+}}],
+          "interactionMap":{
+            "onClick":{
+              "description":"OnClick",
+              "cases":[{
+                  "description":"用例 1",
+                  "isNewIfGroup":false,
+                  "actions":[{
+                      "action":"fadeWidget",
+                      "description":"显示 POO 淡入淡出 1000 毫秒",
+                      "objectsToFades":[{
+                          "objectPath":["fe97c0e65ad24ed093c427c22a879f81"],
+                          "fadeInfo":{
+                            "fadeType":"show",
+                            "options":{
+                              "easing":"fade",
+                              "duration":1000,
+                              "showType":"none"}}}]},
+{
+                      "action":"fadeWidget",
+                      "description":"显示 煩 淡入淡出 1000 毫秒",
+                      "objectsToFades":[{
+                          "objectPath":["e7f70d1597674b18a05b16f5f1bc98da"],
+                          "fadeInfo":{
+                            "fadeType":"show",
+                            "options":{
+                              "easing":"fade",
+                              "duration":1000,
+                              "showType":"none"}}}]},
+{
+                      "action":"fadeWidget",
+                      "description":"显示 悶 淡入淡出 1000 毫秒",
+                      "objectsToFades":[{
+                          "objectPath":["0973bc35f2d84e8293106814ecb39bdd"],
+                          "fadeInfo":{
+                            "fadeType":"show",
+                            "options":{
+                              "easing":"fade",
+                              "duration":1000,
+                              "showType":"none"}}}]},
+{
+                      "action":"fadeWidget",
+                      "description":"显示 悲 淡入淡出 1000 毫秒",
+                      "objectsToFades":[{
+                          "objectPath":["0f8729bc79084827b0798e414cd46933"],
+                          "fadeInfo":{
+                            "fadeType":"show",
+                            "options":{
+                              "easing":"fade",
+                              "duration":1000,
+                              "showType":"none"}}}]},
+{
+                      "action":"fadeWidget",
+                      "description":"显示 憂 淡入淡出 1000 毫秒",
+                      "objectsToFades":[{
+                          "objectPath":["cbc474db28d5449f91d7c15b8e43c9a1"],
+                          "fadeInfo":{
+                            "fadeType":"show",
+                            "options":{
+                              "easing":"fade",
+                              "duration":1000,
+                              "showType":"none"}}}]}]}]}},
+          "tabbable":true,
+          "images":{
+            "normal~":"images/煩/regen/u4.png"}},
+{
+          "id":"3719e750af4e402e99660f42d013d159",
+          "label":"",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":true,
+          "style":{
+            "location":{
+              "x":270,
+              "y":70},
+            "size":{
+              "width":160,
+              "height":110}},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"1f06b27ec0b74cd6839ba90d2d5ad30d",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":true,
+              "style":{
+                "location":{
+                  "x":270,
+                  "y":70},
+                "size":{
+                  "width":160,
+                  "height":110}},
+              "adaptiveStyles":{
+}}],
+          "interactionMap":{
+            "onClick":{
+              "description":"OnClick",
+              "cases":[{
+                  "description":"用例 1",
+                  "isNewIfGroup":false,
+                  "actions":[{
+                      "action":"fadeWidget",
+                      "description":"隐藏 POO 向下滑动 500 毫秒,<br>煩 向下滑动 500 毫秒,<br>悶 向下滑动 500 毫秒,<br>悲 向下滑动 500 毫秒,<br>憂 向下滑动 500 毫秒",
+                      "objectsToFades":[{
+                          "objectPath":["fe97c0e65ad24ed093c427c22a879f81"],
+                          "fadeInfo":{
+                            "fadeType":"hide",
+                            "options":{
+                              "easing":"slideDown",
+                              "duration":500,
+                              "showType":"none"}}},
+{
+                          "objectPath":["e7f70d1597674b18a05b16f5f1bc98da"],
+                          "fadeInfo":{
+                            "fadeType":"hide",
+                            "options":{
+                              "easing":"slideDown",
+                              "duration":500,
+                              "showType":"none"}}},
+{
+                          "objectPath":["0973bc35f2d84e8293106814ecb39bdd"],
+                          "fadeInfo":{
+                            "fadeType":"hide",
+                            "options":{
+                              "easing":"slideDown",
+                              "duration":500,
+                              "showType":"none"}}},
+{
+                          "objectPath":["0f8729bc79084827b0798e414cd46933"],
+                          "fadeInfo":{
+                            "fadeType":"hide",
+                            "options":{
+                              "easing":"slideDown",
+                              "duration":500,
+                              "showType":"none"}}},
+{
+                          "objectPath":["cbc474db28d5449f91d7c15b8e43c9a1"],
+                          "fadeInfo":{
+                            "fadeType":"hide",
+                            "options":{
+                              "easing":"slideDown",
+                              "duration":500,
+                              "showType":"none"}}}]}]}]}},
+          "tabbable":true,
+          "images":{
+            "normal~":"images/煩/regen/u6.png"}},
+{
+          "id":"e7f70d1597674b18a05b16f5f1bc98da",
+          "label":"煩",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":false,
+          "style":{
+            "location":{
+              "x":324,
+              "y":214},
+            "size":{
+              "width":76,
+              "height":65},
+            "visible":false},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"af5e39a1f8004ee38a065a18d486cc60",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":false,
+              "style":{
+                "location":{
+                  "x":324,
+                  "y":214},
+                "size":{
+                  "width":76,
+                  "height":65},
+                "visible":false},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/煩/regen/煩_u8.png"}},
+{
+          "id":"0973bc35f2d84e8293106814ecb39bdd",
+          "label":"悶",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":false,
+          "style":{
+            "location":{
+              "x":259,
+              "y":230},
+            "size":{
+              "width":55,
+              "height":47},
+            "visible":false},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"5e210b620fc7448fbb3787e3da4634ce",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":false,
+              "style":{
+                "location":{
+                  "x":259,
+                  "y":230},
+                "size":{
+                  "width":55,
+                  "height":47},
+                "visible":false},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/煩/regen/悶_u10.png"}},
+{
+          "id":"0f8729bc79084827b0798e414cd46933",
+          "label":"悲",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":false,
+          "style":{
+            "location":{
+              "x":290,
+              "y":218},
+            "size":{
+              "width":40,
+              "height":34},
+            "visible":false},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"eafe5e3c8a6b4e108ca93500e5fa1ec8",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":false,
+              "style":{
+                "location":{
+                  "x":290,
+                  "y":218},
+                "size":{
+                  "width":40,
+                  "height":34},
+                "visible":false},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/煩/regen/悲_u12.png"}},
+{
+          "id":"cbc474db28d5449f91d7c15b8e43c9a1",
+          "label":"憂",
+          "type":"imageBox",
+          "styleType":"imageBox",
+          "visible":false,
+          "style":{
+            "location":{
+              "x":306,
+              "y":214},
+            "size":{
+              "width":39,
+              "height":34},
+            "visible":false},
+          "adaptiveStyles":{
+},
+          "objects":[{
+              "id":"b7289bb34c98401d805f0d8d7f39a2be",
+              "label":"",
+              "isContained":true,
+              "type":"richTextPanel",
+              "styleType":"paragraph",
+              "visible":false,
+              "style":{
+                "location":{
+                  "x":306,
+                  "y":214},
+                "size":{
+                  "width":39,
+                  "height":34},
+                "visible":false},
+              "adaptiveStyles":{
+}}],
+          "images":{
+            "normal~":"images/煩/regen/憂_u14.png"}}]}},
+  "masters":{
+},
+  "objectPaths":{
+    "fe97c0e65ad24ed093c427c22a879f81":{
+      "scriptId":"u0"},
+    "bbd8b57a55154984a164efb41ba06ea6":{
+      "scriptId":"u1"},
+    "4bcc7b8df02e44109d882218b7236041":{
+      "scriptId":"u2"},
+    "e07c9c65bfa14745b71122ebeae0a500":{
+      "scriptId":"u3"},
+    "7fafc98146bd4c979b83a5b2d3af0b9d":{
+      "scriptId":"u4"},
+    "7dec9b7a2a9f4aaebd412a6f2b64a934":{
+      "scriptId":"u5"},
+    "3719e750af4e402e99660f42d013d159":{
+      "scriptId":"u6"},
+    "1f06b27ec0b74cd6839ba90d2d5ad30d":{
+      "scriptId":"u7"},
+    "e7f70d1597674b18a05b16f5f1bc98da":{
+      "scriptId":"u8"},
+    "af5e39a1f8004ee38a065a18d486cc60":{
+      "scriptId":"u9"},
+    "0973bc35f2d84e8293106814ecb39bdd":{
+      "scriptId":"u10"},
+    "5e210b620fc7448fbb3787e3da4634ce":{
+      "scriptId":"u11"},
+    "0f8729bc79084827b0798e414cd46933":{
+      "scriptId":"u12"},
+    "eafe5e3c8a6b4e108ca93500e5fa1ec8":{
+      "scriptId":"u13"},
+    "cbc474db28d5449f91d7c15b8e43c9a1":{
+      "scriptId":"u14"},
+    "b7289bb34c98401d805f0d8d7f39a2be":{
+      "scriptId":"u15"}}});
